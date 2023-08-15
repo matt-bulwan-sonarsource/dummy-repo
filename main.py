25ffe625f202cf44d1415ab285bd0364807c6631
Matt Bulwan <matt.bulwan@sonarsource.com>
2023-08-15 16:08:13 -0500

    f3 conflict resolution during rebase

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main(event) -> None:
     f2 added before line 1
+    f3 doing something
     line 1
     f2 added before line 2
+    f3 doing something else
     line 2
     f2 added before line 3
+    f3 doing something else
     line 3
 
